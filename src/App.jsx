3adddd87af8ba0bb70a545ca332369e0dad680ca
MustafaGaber1 <mostafagaber657@gmail.com>
2025-04-09 23:30:26 +0200

    Fix image paths

--- src/App.jsx
+++ src/App.jsx
@@ -51,7 +51,7 @@ const App = () => {
       }
       setMovieList(data.results || []);
 
-      if (query && data.results.length > 0) {
+      if (query && data.length > 0) {
         await updateSearchCount(query, data.results[0]);
       }
     } catch (error) {
@@ -84,7 +84,7 @@ const App = () => {
       <div className="pattern" />
       <div className="wrapper">
         <header>
-          <img src="../public/hero.png" alt="Hero Banner" />
+          <img src="/hero.png" alt="Hero Banner" />
           <h1>
             Find <span className="text-gradient">Movies</span> You'll Enjoy
             Without the Hassle
